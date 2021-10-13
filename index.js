3faad9f8322f46eb97593be795394030cc62d41b
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-10-13 16:54:12 -0400

    finished task 1

--- index.js
+++ index.js
@@ -9,16 +9,15 @@
 
 // EXAMPLE SOLUTION CODE:
 function Airplane(name) {
-  this.name = name;
-  this.isFlying = false;
+	this.name = name;
+	this.isFlying = false;
 }
 Airplane.prototype.takeOff = function () {
-  this.isFlying = true;
+	this.isFlying = true;
 };
 Airplane.prototype.land = function () {
-  this.isFlying = false;
+	this.isFlying = false;
 };
-
 
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -39,15 +38,38 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+	this.stomach = [];
 }
 
+Person.prototype.eat = function (edible) {
+	if (this.stomach.length < 10) {
+		this.stomach.push(edible);
+	}
+};
 
+Person.prototype.poop = function () {
+	this.stomach = [];
+};
 
+Person.prototype.toString = function () {
+	return `${this.name}, ${this.age}`;
+};
 
+const Jon = new Person('Jon', 34);
+const Brit = new Person('Brit', 34);
+const Latoya = new Person('Latoya', 32);
 
+console.log('task 1', Jon.toString());
 
+Jon.eat('pizza');
+
+console.log('Jon stomach', Jon.stomach);
+console.log(Jon.poop());
+
+console.log('Jon stomach after using the bathroom', Jon.stomach);
 
 /*
   TASK 2
@@ -63,10 +85,7 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
-}
-
+function Car() {}
 
 /*
   TASK 3
@@ -75,10 +94,7 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
-}
-
+function Baby() {}
 
 /* 
   TASK 4
@@ -89,18 +105,17 @@ function Baby() {
   4. 
 */
 
-
 ///////// END OF CHALLENGE /////////
 
 /* 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑🛑🛑🛑🛑🛑 */
-function foo(){
-  console.log('its working!');
-  return 'bar';
+function foo() {
+	console.log('its working!');
+	return 'bar';
 }
 foo();
 module.exports = {
-  foo,
-  Person, 
-  Car,
-  Baby
-}
+	foo,
+	Person,
+	Car,
+	Baby
+};
