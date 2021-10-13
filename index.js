00a64ab2f3bdc5c788ee43b217485dd79467f0bd
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-10-13 17:29:45 -0400

    finished task 2

--- index.js
+++ index.js
@@ -85,7 +85,16 @@ console.log('Jon stomach after using the bathroom', Jon.stomach);
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {}
+function Car(model, milesPerGallon) {
+	this.model = model;
+	this.milesPerGallon = milesPerGallon;
+	this.tank = 0;
+	this.odometer = 0;
+}
+
+Car.prototype.fill = function (gallons) {
+	this.tank = this.tank + gallons;
+};
 
 /*
   TASK 3
